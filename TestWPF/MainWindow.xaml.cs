09d1a6ec440608379c147f492bff5d2ada107fee
ALI <kradunmaker@gmail.com>
2021-08-06 18:45:55 +0500

    Add project files.

---
+++ TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using FileSystemVisitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Finder finder = new Finder();        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                finder.GetFiles(tbPath.Text, tbExtension.Text);
+                finder.Started += () => Onstarted();
+                finder.OnStarted();
+                lbItems.ItemsSource = finder.Files;
+                finder.Finishied += () => OnFinished();
+                finder.OnFinished();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnFinished()
+        {
+            MessageBox.Show("Finished");
+        }
+
+        private void Onstarted()
+        {
+            MessageBox.Show("Started");
+        }
+    }
+}
